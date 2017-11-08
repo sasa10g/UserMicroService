f0ef43329c843ea1b0bb37e0be4ab43bc47cee63
Sasa10 <sasagvozdenovic@hotmail.com>
2017-11-08 17:07:33 +0100

    test v.1

---
+++ UserMicroService/DataAccess/UserDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UserMicroService.Models;
+
+
+namespace UserMicroService.DataAccess
+{
+    public static class UserDB
+    {
+        public static List<User> usersInMemory = new List<User>();
+
+        //vracanje svih korisnika
+        public static List<User> GetAllUsers(){ //dodavanje filtera
+            return usersInMemory;
+        }
+
+        public static User GetUserById(int id){
+            foreach (User user in usersInMemory){
+                if (user.Id == id){
+                    return user;
+                }
+                //linq - zamjena za foreach, provjeriti
+            }
+            return null;
+        }
+
+
+        public static User CreateUser(User user){
+            usersInMemory.Add(user);
+            return GetUserById(user.Id);
+        }
+
+
+    }
+}
